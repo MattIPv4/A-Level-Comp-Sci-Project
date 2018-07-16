886ef86edf0d222107933e9730fae7a7f5eaaf61
MattIPv4 <matthew@cowley.org.uk>
2018-07-16 13:28:35 +0100

    use non deprecated stuff

--- slst-rota/app/m_auth/forms.py
+++ slst-rota/app/m_auth/forms.py
@@ -1,9 +1,9 @@
-from flask.ext.wtf import Form  # Form
+from flask_wtf import FlaskForm # Form
 from wtforms import StringField, PasswordField  # Form: Elements
 from wtforms.validators import DataRequired  # Form: Validation
 
 
 # Login (using WTForms)
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     username = StringField('Username', [DataRequired(message='Please enter your username.')])
     password = PasswordField('Password', [DataRequired(message='Please enter your password.')])
