28a0f7dccff2bac0b60eafd1358761d3e934c4be
MattIPv4 <matthew@cowley.org.uk>
2018-08-01 20:30:41 +0100

    staff rota view

--- slst-rota/app/modules/staff/controllers.py
+++ slst-rota/app/modules/staff/controllers.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash  # Flask
 from werkzeug.security import generate_password_hash  # Passwords
+import calendar # Calendar
 
 from app import db_session, error_render  # DB, Errors
 from app.modules import auth  # Auth
 from app.modules.staff.forms import AccountForm  # Forms
+from app.modules.student import Session # Session
 from app.modules.auth import User # User
 
 # Blueprint
@@ -58,6 +60,107 @@ def new_account():
     user, error = auth_check()
     if error:
         return error
+
+    # Form
+    form = AccountForm(request.form)
+
+    # Verify the form
+    if form.validate_on_submit():
+
+        # Verify username
+        if form.username.data:
+
+            result = User.query.filter_by(username=form.username.data).first()
+
+            # Verify username not already used
+            if not result:
+
+                # Verify password
+                if form.password.data:
+
+                    # Verify auth level
+                    if form.auth_level.data:
+
+                        session = db_session()
+                        user = User(form.username.data, generate_password_hash(form.password.data), form.auth_level.data)
+                        session.add(user)
+                        session.commit()
+                        return redirect(url_for('staff.accounts'))
+
+                    else:
+                        flash('Please select an auth level')
+
+                else:
+                    flash('Please enter a password')
+
+            else:
+                flash('Username {} already in use'.format(form.username.data))
+
+        else:
+            flash('Please enter a username')
+
+    # Render
+    return render_template("staff/new_account.jinja2", form=form)
+
+
+# Rota view
+@staff.route('/rota/', methods=['GET'])
+def rota():
+    user, error = auth_check()
+    if error:
+        return error
+
+    # Get rota data
+    data = Session.query.filter_by(archived=False).order_by(Session.day.asc(), Session.start_time.asc()).all()
+    rota_data = []
+    current_day = None
+    for session in data:
+        # Day subheadings
+        if session.day != current_day:
+            rota_data.append([False, [list(calendar.day_name)[session.day], "", "", ""]])
+            current_day = session.day
+
+        # Assignments for day (highlight ones with current user)
+        rota_data.append([
+            (session.assignments == []),
+            [
+                session.start_time_frmt,
+                session.end_time_frmt,
+                ", ".join([f.user.username for f in session.assignments]) or "None",
+                "<a href='{}' class='button'>Edit Session</a>".format(url_for("staff.rota_edit_session", id=session.id)) +
+                " &nbsp; <a href='{}' class='button'>Update Assignments</a>".format(url_for("staff.rota_edit_assignments", id=session.id))
+            ]
+        ])
+
+    return render_template("staff/rota.jinja2", rota_data=rota_data)
+
+# Rota edit - session
+@staff.route('/rota/edit/session/<int:id>', methods=['GET', 'POST'])
+def rota_edit_session(id: int):
+    user, error = auth_check()
+    if error:
+        return error
+
+    ## TODO: below
+
+# Rota edit - assignments
+@staff.route('/rota/edit/assignments/<int:id>', methods=['GET', 'POST'])
+def rota_edit_assignments(id: int):
+    user, error = auth_check()
+    if error:
+        return error
+
+    ## TODO: below
+
+
+# New rota session
+@staff.route('/rota/new', methods=['GET', 'POST'])
+def rota_new():
+    user, error = auth_check()
+    if error:
+        return error
+
+    ## TODO: below
 
     # Form
     form = AccountForm(request.form)
